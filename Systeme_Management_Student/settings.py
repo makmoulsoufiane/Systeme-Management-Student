e75d6d89648f66c57f81b42b8f1faa7a3fd7aaeb
Boukabouya <boukabouyarayane2019@gmail.com>
2024-08-09 00:48:16 +0100

    Create a teacher table and migrate it with Django

--- Systeme_Management_Student/settings.py
+++ Systeme_Management_Student/settings.py
@@ -72,11 +72,14 @@ WSGI_APPLICATION = 'Systeme_Management_Student.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'system_management_student',   # The name of the database you created
+        'USER': 'user',      # The MySQL user you want to use
+        'PASSWORD': 'password', # Password for the MySQL user
+        'HOST': 'localhost',            # Typically 'localhost'
+        'PORT': '3306',                 # Default MySQL port
     }
 }
 

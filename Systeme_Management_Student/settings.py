7bb76b31a3afae089eba3b2b41ee48425e29e981
makmoul soufiane <makmoulsoufiane2@gmail.com>
2024-08-14 20:49:21 +0100

    solving problem manuly file setting.py

--- Systeme_Management_Student/settings.py
+++ Systeme_Management_Student/settings.py
@@ -72,29 +72,11 @@ WSGI_APPLICATION = 'Systeme_Management_Student.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-DATABASES = {
 
-""" DATABASES = {
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'system_management_student',   # The name of the database you created
-        'USER': 'root',      # The MySQL user you want to use
-        'PASSWORD': '', # Password for the MySQL user
-        'HOST': '127.0.0.1',            # Typically 'localhost'
-        'PORT': '',                 # Default MySQL port
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
-} """
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Systeme_Management_Student',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '',
     }
 }
 

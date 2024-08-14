e9eb7d6d952f4b9d6046c7f77ffbab88eec622cd
makmoul soufiane <makmoulsoufiane2@gmail.com>
2024-08-14 20:45:10 +0100

    Revert "Merge branch 'develop' into feature/create-teacher-table"
    This reverts commit e4926477a7241adc154674699d6ebcfdb7252023, reversing
    changes made to bcab32ef7858f9db7dceec8d5c0b8f0ac4300309.

--- Systeme_Management_Student/settings.py
+++ Systeme_Management_Student/settings.py
@@ -72,9 +72,16 @@ WSGI_APPLICATION = 'Systeme_Management_Student.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+DATABASES = {
 
 """ DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'system_management_student',   # The name of the database you created
+        'USER': 'root',      # The MySQL user you want to use
+        'PASSWORD': '', # Password for the MySQL user
+        'HOST': '127.0.0.1',            # Typically 'localhost'
+        'PORT': '',                 # Default MySQL port
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
@@ -90,6 +97,7 @@ DATABASES = {
         'PORT': '',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
